076c81bbbf3bbc48aff6f87ac2e0cc057e5aed85
alexlibre <alexeration@gmail.com>
2020-06-17 18:20:07 +0300

    added build to gh-pages

---
+++ build/precache-manifest.9ebf83d09ed86f9a48981bd530e637b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "565aec046e7e834e28ec",
+    "url": "/static/css/main.d5958c39.chunk.css"
+  },
+  {
+    "revision": "565aec046e7e834e28ec",
+    "url": "/static/js/main.565aec04.chunk.js"
+  },
+  {
+    "revision": "789b1b5576225a7d3213",
+    "url": "/static/js/1.789b1b55.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b6a84511b077d878705c9eba0653447",
+    "url": "/index.html"
+  }
+];
